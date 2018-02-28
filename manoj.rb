9b299482c2a69dad0d95ae2452e7d893fc61283f
Manoj Panta <manojpanta95@gmail.com>
2018-02-27 19:24:50 -0700

    testing for hash

--- manoj.rb
+++ manoj.rb
@@ -11,15 +11,21 @@ class Human
 
 
 
-def test
-a = [1,2,3,4,5,6,7,8,9]
-  a.reduce(0) do |result, num|
+  def test
+    a = [1,2,3,4,5,6,7,8,9]
+    a.reduce(0) do |result, num|
     result += num
     result
+    end
   end
-end
 
-# def test_again
+  def test_again
+    a = ["manoj", "menuka", "mandir", "bhim", "dillumaya"]
+    a.reduce({}) do |family, names|
+    family[names.capitalize] = names.length
+    family
+    end
+  end
 
 end
 
@@ -29,3 +35,4 @@ manoj = Human.new("manoj", 22)
 puts manoj.test
 puts manoj.name
 puts manoj.age
+puts manoj.test_again
