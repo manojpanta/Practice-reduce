7b5933083e87fc6e99c6b4bbf84f480491c85151
Manoj Panta <manojpanta95@gmail.com>
2018-02-27 19:17:41 -0700

    playing with reduce

--- manoj.rb
+++ manoj.rb
@@ -1,15 +1,31 @@
 class Human
 
 
-  attr_reader :name
+  attr_reader :name, :age
 
-  def initialize(name)
+  def initialize(name, age)
     @name = name.upcase
+    @age = age
   end
 
+
+
+
+def test
+a = [1,2,3,4,5,6,7,8,9]
+  a.reduce(0) do |result, num|
+    result += num
+    result
+  end
+end
+
+# def test_again
 
 end
 
 
-manoj = Human.new("manoj")
+
+manoj = Human.new("manoj", 22)
+puts manoj.test
 puts manoj.name
+puts manoj.age
